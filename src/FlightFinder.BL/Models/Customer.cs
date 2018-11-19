9046c267c2244ab29fde5df5fd87d17fdd88fa02
Martin Dybal <martin@dybal.it>
2018-11-19 22:23:08 +0100

    Move types to matching types

---
+++ src/FlightFinder.BL/Models/Customer.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FlightFinder.BL.Models
+{
+    public class Customer
+    {
+        [Required, MinLength(6)]
+        public string Name { get; set; }
+
+        [Required, EmailAddress]
+        public string Email { get; set; }
+    }
+}
--- src/FlightFinder.BL/Models/Order.cs
+++ src/FlightFinder.BL/Models/Order.cs
@@ -1,18 +1,7 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace FlightFinder.BL.Models
+﻿namespace FlightFinder.BL.Models
 {
     public class Order
     {
         public Customer Customer { get; set; } = new Customer();
-    }
-
-    public class Customer
-    {
-        [Required, MinLength(6)]
-        public string Name { get; set; }
-
-        [Required, EmailAddress]
-        public string Email { get; set; }
     }
 }
