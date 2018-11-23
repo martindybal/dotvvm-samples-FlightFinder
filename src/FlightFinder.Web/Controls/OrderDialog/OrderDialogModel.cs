3d44b77de237469a586d3ab8efd93a2c2282842a
Martin Dybal <martin@dybal.it>
2018-11-23 10:36:32 +0100

    Finishing demo

--- src/FlightFinder.Web/Controls/OrderDialog/OrderDialogModel.cs
+++ src/FlightFinder.Web/Controls/OrderDialog/OrderDialogModel.cs
@@ -6,5 +6,14 @@ namespace FlightFinder.Web.Controls.OrderDialog
     {
         public Order Order { get; set; } = new Order();
         public bool DialogIsOpen { get; set; }
+
+        public bool ResultDialogIsOpen { get; set; }
+        public string ResultMessage { get; set; }
+
+        public void Next()
+        {
+            ResultDialogIsOpen = true;
+            ResultMessage = "Thank you!";
+        }
     }
 }
